49b381b98022f6c70d9ad9cea05e5e8f66003be2
Karaliang <j20030811@gmail.com>
2024-05-30 15:06:02 +0800

    環境建置及首頁

--- stockanalysis/settings.py
+++ stockanalysis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home.apps.HomeConfig",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,8 @@ ROOT_URLCONF = "stockanalysis.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates').replace('\\', '/')
+],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -74,9 +77,13 @@ WSGI_APPLICATION = "stockanalysis.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  #PostgreSQL
+        'NAME': 'Stocknews',  #資料庫名稱
+        'USER': 'postgres',  #資料庫帳號
+        'PASSWORD': '0811',  #資料庫密碼
+        'HOST': 'localhost',  #Server(伺服器)位址
+        'PORT': '5433'  #PostgreSQL Port號
     }
 }
 
